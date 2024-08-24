5bd9232636515ba5b3b9681effdcce26ffad0741
rajarya199 <aryalrajan078@gmail.com>
2024-08-24 15:29:27 +0545

    cb fn2

--- callback2.js
+++ callback2.js
@@ -8,6 +8,16 @@ const getData=(dataId,getNextData)=>{
 
     },2000)
 }
+// getData(4, ()=>{
+//     getData(6)
+// })
+// output 
+//       {wait 2s}  data 4  ( wait2s)  data 6
+
 getData(4, ()=>{
-    getData(6)
+    getData(6,()=>{
+        getData(7,()=>{
+            getData(9)
+        })
+    })
 })
